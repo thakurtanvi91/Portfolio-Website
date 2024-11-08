df442db01cfd190abd4a414da1f75c501f18be3f
thakurtanvi91 <161221929+thakurtanvi91@users.noreply.github.com>
2024-11-08 16:50:16 +0100

    Add files via upload

---
+++ action.js
@@ -0,0 +1,9 @@
+<script>
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
